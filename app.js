3aab80ec7ade0f74342b764c174dda288a46badf
Abdi Majid <abdi.amajid.dev>
2023-01-22 22:20:54 +0000

    add new task complete

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const chevronDown = document.querySelector(".chevronDown");
 const MobileSideBar = document.querySelector(".SideBar");
+const TaskPopUp = document.querySelector(".addTaskPopUp");
+
+
+window.addEventListener('click', function(e){   
+    if(TaskPopUp.contains(e.target)){
+        console.log("working")
+    } else{
+        console.log("not")
+    }
+})
+
 
 chevronDown.addEventListener("click", function(){
 MobileSideBar.classList.toggle("displayBlock");
